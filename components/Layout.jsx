152f1d04311decf5a9bfc42a4a45bfe59402b4b3
viistorrr <victormeza41@gmail.com>
2022-07-28 14:53:32 -0500

    meta head

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -5,6 +6,14 @@ import Header from "./Header";
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>viistorrr - Software Engineer - IT Consultant</title>
+        <meta name="description" content="Software Engineer - IT Consultant" />
+        <meta name="keywords" content="Software Engineer, IT Consultant" />
+        <meta name="author" content="Software Engineer - IT Consultant" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="relative pt-6 pb-16 sm:pb-24">
         <Header />
         {children}
