32499d31847df8c5dd02d2fb9bc2621619383fa8
viistorrr <victormeza41@gmail.com>
2022-09-30 05:39:38 -0500

    add slack channel icon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -14,7 +14,6 @@ export default function Document() {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
             gtag('config', 'G-8XLJF2LSLS');
             `}
         </Script>
