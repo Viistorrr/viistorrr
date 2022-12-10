35e7b92c7c3a2c0e5605502846fd168c2fd1492e
viistorrr <victormeza41@gmail.com>
2022-12-09 19:26:25 -0500

    changing card icons

--- src/components/concepts/Details/index.tsx
+++ src/components/concepts/Details/index.tsx
@@ -10,8 +10,16 @@ import {
   //MagnifyingGlassIcon,
   PlusIcon,
   ShareIcon,
-  StarIcon,
 } from "@heroicons/react/solid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEllipsisVertical,
+  faThumbsUp,
+  faComment,
+  faEye,
+  faShare,
+} from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 const questions = [
   {
@@ -80,88 +88,6 @@ const Details = () => {
                       </a>
                     </p>
                   </div>
-                  <div className="flex flex-shrink-0 self-center">
-                    <Menu as="div" className="relative inline-block text-left">
-                      <div>
-                        <Menu.Button className="-m-2 flex items-center rounded-full p-2 text-gray-400 hover:text-gray-600">
-                          <span className="sr-only">Open options</span>
-                          <EyeIcon className="h-5 w-5" aria-hidden="true" />
-                        </Menu.Button>
-                      </div>
-
-                      <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                      >
-                        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          <div className="py-1">
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  href="#"
-                                  className={classNames(
-                                    active
-                                      ? "bg-gray-100 text-gray-900"
-                                      : "text-gray-700",
-                                    "flex px-4 py-2 text-sm"
-                                  )}
-                                >
-                                  <StarIcon
-                                    className="mr-3 h-5 w-5 text-gray-400"
-                                    aria-hidden="true"
-                                  />
-                                  <span>Add to favorites</span>
-                                </a>
-                              )}
-                            </Menu.Item>
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  href="#"
-                                  className={classNames(
-                                    active
-                                      ? "bg-gray-100 text-gray-900"
-                                      : "text-gray-700",
-                                    "flex px-4 py-2 text-sm"
-                                  )}
-                                >
-                                  <EyeIcon
-                                    className="mr-3 h-5 w-5 text-gray-400"
-                                    aria-hidden="true"
-                                  />
-                                  <span>Embed</span>
-                                </a>
-                              )}
-                            </Menu.Item>
-                            <Menu.Item>
-                              {({ active }) => (
-                                <a
-                                  href="#"
-                                  className={classNames(
-                                    active
-                                      ? "bg-gray-100 text-gray-900"
-                                      : "text-gray-700",
-                                    "flex px-4 py-2 text-sm"
-                                  )}
-                                >
-                                  <FlagIcon
-                                    className="mr-3 h-5 w-5 text-gray-400"
-                                    aria-hidden="true"
-                                  />
-                                  <span>Report content</span>
-                                </a>
-                              )}
-                            </Menu.Item>
-                          </div>
-                        </Menu.Items>
-                      </Transition>
-                    </Menu>
-                  </div>
                 </div>
                 <h2
                   id={"question-title-" + question.id}
@@ -174,6 +100,14 @@ const Details = () => {
                 className="mt-2 space-y-4 text-sm text-gray-700"
                 dangerouslySetInnerHTML={{ __html: question.body }}
               />
+              <div className="mt-2 ">
+                <Link href="/blog">
+                  <a className="font-bold text-primary text-xl hover:text-red">
+                    Leer más...
+                  </a>
+                </Link>
+              </div>
+
               <div className="mt-6 flex justify-between space-x-8">
                 <div className="flex space-x-6">
                   <span className="inline-flex items-center text-sm">
@@ -181,7 +115,7 @@ const Details = () => {
                       type="button"
                       className="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
                     >
-                      <EyeIcon className="h-5 w-5" aria-hidden="true" />
+                      <FontAwesomeIcon icon={faThumbsUp} size="xl" />
                       <span className="font-medium text-gray-900">
                         {question.likes}
                       </span>
@@ -193,7 +127,7 @@ const Details = () => {
                       type="button"
                       className="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
                     >
-                      <EyeIcon className="h-5 w-5" aria-hidden="true" />
+                      <FontAwesomeIcon icon={faComment} size="xl" />
                       <span className="font-medium text-gray-900">
                         {question.replies}
                       </span>
@@ -205,7 +139,7 @@ const Details = () => {
                       type="button"
                       className="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
                     >
-                      <EyeIcon className="h-5 w-5" aria-hidden="true" />
+                      <FontAwesomeIcon icon={faEye} size="xl" />
                       <span className="font-medium text-gray-900">
                         {question.views}
                       </span>
@@ -219,7 +153,7 @@ const Details = () => {
                       type="button"
                       className="inline-flex space-x-2 text-gray-400 hover:text-gray-500"
                     >
-                      <ShareIcon className="h-5 w-5" aria-hidden="true" />
+                      <FontAwesomeIcon icon={faShare} size="xl" />
                       <span className="font-medium text-gray-900">Share</span>
                     </button>
                   </span>
