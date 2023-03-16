18b9e99776472827d08ef7db65d9b4ef69b54bc0
viistorrr <victormeza41@gmail.com>
2023-03-16 06:14:09 -0500

    update portada

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,14 +7,12 @@ const Home = () => {
       <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
         <div className="text-center">
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block xl:inline">frontend</span>
+            <span className="block text-primary xl:inline">dev</span>
             <span className="block xl:inline ">{" | "}</span>
-            <span className="block xl:inline">dev</span>
+            <span className="block xl:inline">Ingeniero</span>
             {" | "}
-            <span className="block text-primary xl:inline">IT Consultant</span>
+            <span className="block text-primary xl:inline">Software</span>
           </h1>
-
-          <p className="mt-6 text-secondary text-lg">Software Engineer</p>
         </div>
       </main>
     </Layout>
